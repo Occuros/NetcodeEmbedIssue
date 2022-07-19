20f5023c1420901883f6826f4406a1e812106ecc
Roger Küng <roger.kueng@holonautic.com>
2022-07-19 17:36:37 +0200

    Minimal code added

---
+++ Assets/Scripts/Bootstrap/GameBootstrap.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.NetCode;
+using Unity.Networking.Transport;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [UnityEngine.Scripting.Preserve]
+    public class GameBootstrap: ClientServerBootstrap
+    {
+        public override bool Initialize(string defaultWorldName)
+        {
+            AutoConnectPort = 7979;
+            var server_ip = "127.0.0.1";
+
+            DefaultConnectAddress = NetworkEndPoint.Parse(server_ip, 7979);
+            var result = base.Initialize(defaultWorldName);
+            Debug.Log($"We are connecting to the server {server_ip}");
+           
+            return result;
+        }
+        
+       
+    }
+}
---
+++ Assets/Scripts/Server/GoInGameServerSystem.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.NetCode;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [UpdateInGroup(typeof(ServerSimulationSystemGroup))]
+    public partial class GoInGameServerSystem : SystemBase
+    {
+        protected override void OnCreate()
+        {
+            RequireSingletonForUpdate<CubeSpawner>();
+            RequireForUpdate(GetEntityQuery(
+                ComponentType.ReadOnly<GoInGameRequest>(),
+                ComponentType.ReadOnly<ReceiveRpcCommandRequestComponent>()));
+        }
+
+        protected override void OnUpdate()
+        {
+            Entities
+                .WithNone<SendRpcCommandRequestComponent>()
+                .WithoutBurst()
+                .WithStructuralChanges()
+                .ForEach((Entity requestEntity, in GoInGameRequest request,
+                    in ReceiveRpcCommandRequestComponent received) =>
+                {
+                    EntityManager.AddComponent<NetworkStreamInGame>(received.SourceConnection);
+                    var networkId = GetComponent<NetworkIdComponent>(received.SourceConnection).Value;
+                    Debug.Log($"Server setting connection {networkId} to in game");
+                    EntityManager.DestroyEntity(requestEntity);
+                }).Run();
+                
+        }
+    }
+}
